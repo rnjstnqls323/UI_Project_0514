ddb421768bcc6a55ab631f5727b78ec9b380da51
Bhs1515 <89412076+Bhs1515@users.noreply.github.com>
2025-05-16 17:51:09 +0900

    11
    111

--- UI_Project_0514/UI_Project_0514/Objects/Player/Player.h
+++ UI_Project_0514/UI_Project_0514/Objects/Player/Player.h
@@ -4,6 +4,11 @@ class Player : public Singleton<Player>
 {
 	friend Singleton;
 
-	// 플레이어는 아이템 정보 다 들고있기?
+	// 플레이어는 아이템 정보 다 들고있기 노노 스토어panel에서 구매한 정보만 들고잇기!
+private:
+	Player();
+	~Player();
 
+public :
+	void AddItem();
 };
